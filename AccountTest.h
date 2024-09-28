6dd053b93daa70fd60aa25f6116d382b9d5c1edb
Bohdan Shevchenko <104861557+Bohdanken@users.noreply.github.com>
2024-09-28 14:38:07 +0100

    Account tests for test-driven development

---
+++ AccountTest.h
@@ -0,0 +1,8 @@
+#pragma once
+void testCreateAccount();
+void testCreateAccountWithInvalidBalance();
+void testCreateAccountWithInvalidName();
+void testDeposit();
+void testWithdrawSufficientFunds();
+void testWithdrawInsufficientFunds();
+void testCreateAccountWithDuplicateNumber();
