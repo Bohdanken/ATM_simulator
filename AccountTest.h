4348c83b24cb4766ac639d2c27cce28c8c83ceb6
Oleksiy Zhytnetsky <armageddon.twink2@gmail.com>
2024-10-12 12:49:50 +0300

    Set up cmake project for tests, update gitignore

--- AccountTest.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-void testCreateAccount();
-void testCreateAccountWithInvalidBalance();
-void testCreateAccountWithInvalidName();
-void testDeposit();
-void testWithdrawSufficientFunds();
-void testWithdrawInsufficientFunds();
-void testCreateAccountWithDuplicateNumber();
---
+++ TestsProject/TestsProject.cpp
@@ -0,0 +1,12 @@
+﻿// TestsProject.cpp : Defines the entry point for the application.
+//
+
+#include "TestsProject.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
