4f5bfea9b671c49defecaeecb92aef4c0625b849
Oleksiy Zhytnetsky <oleksii.zhytnetsky@gmail.com>
2024-10-14 22:36:53 +0300

    Join feature_tests & feature_boost_asio into feature_t1_final

--- TestsProject/main.cpp
+++ TestsProject/main.cpp
@@ -1,12 +1,14 @@
 ﻿#include "AccountTest.h"
 #include "CardTest.h"
 #include "TransferTest.h"
+#include "TransferDaemonTest.h"
 
 int main()
 {
 	runAccountTestSuite();
 	runCardTestSuite();
 	runTransferTestSuite();
+	testTransferDaemon();
 
 	return 0;
 }
