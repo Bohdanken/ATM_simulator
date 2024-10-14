8ed6a47fad6583cc4cf9be38b52c350792642089
Oleksiy Zhytnetsky <oleksii.zhytnetsky@gmail.com>
2024-10-14 15:34:30 +0300

    Save progress [WIP]

---
+++ TestsProject/TransferTest.h
@@ -0,0 +1,18 @@
+#pragma once
+#ifndef TRANSFER_TEST_H
+#define TRANSFER_TEST_H
+
+#pragma region Utils
+void trasferTestWrapper(void (*const)());
+#pragma endregion
+
+#pragma region Tests declaraction
+// ...
+#pragma endregion
+
+inline void runTransferTestSuite()
+{
+    // ...
+}
+
+#endif // !TRANSFER_TEST_H
---
+++ TestsProject/mocks/TransferMocks.h
@@ -0,0 +1,10 @@
+#pragma once
+#ifndef TRANSFER_MOCKS_H
+#define TRANSFER_MOCKS_H
+
+struct TransferEntity
+{
+
+};
+
+#endif // !TRANSFER_MOCKS_H
