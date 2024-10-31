d0e6c2a310aecfb45a65d8e1e20ddfb49dc049c6
Oleksiy Zhytnetsky <oleksii.zhytnetsky@gmail.com>
2024-10-31 02:04:44 +0200

    Save project config/main file; Prepare to set up git submodules [WIP]

--- ATM-Frontend/main.cpp
+++ ATM-Frontend/main.cpp
@@ -1,8 +1,110 @@
+#include <GLFW/glfw3.h>
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
+
 #include <iostream>
+
+// Initialise Imgui
+void setupImGui(GLFWwindow *const window)
+{
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO &io = ImGui::GetIO(); 
+    (void)io; // In case the IO is not used
+
+    // Setup Dear ImGui style
+    ImGui::StyleColorsDark();
+
+    // Setup Platform/Renderer bindings
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+
+    // Specify your OpenGL version
+    ImGui_ImplOpenGL3_Init("#version 460"); 
+}
+
+// Cleanup ImGui
+void cleanupImGui()
+{
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+}
+
+// 
+void renderUI()
+{
+    ImGui::Text("Hello, ImGui!");
+    ImGui::SliderFloat("Float", new float(0.0f), 0.0f, 1.0f);
+}
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    // Initialise GLFW
+    if (!glfwInit())
+    {
+        std::cerr << "Failed to initialize GLFW" << std::endl;
+        return -1;
+    }
 
+    // Specify OpenGL version & operation mode
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    // Create a windowed mode window and its OpenGL context
+    GLFWwindow *window = glfwCreateWindow(1600, 900, "ImGui + GLFW Example", nullptr, nullptr);
+    if (!window)
+    {
+        std::cerr << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1); // Enable vsync
+
+    std::cout << "OpenGL Version: " << glGetString(GL_VERSION) << std::endl;
+    std::cout << "Vendor: " << glGetString(GL_VENDOR) << std::endl;
+    std::cout << "Renderer: " << glGetString(GL_RENDERER) << std::endl;
+
+    // Setup ImGui
+    setupImGui(window);
+
+    // Main loop
+    while (!glfwWindowShouldClose(window))
+    {
+        // Poll and handle events
+        glfwPollEvents();
+
+        // Start the ImGui frame
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        // Render your UI
+        renderUI();
+
+        // Rendering
+        ImGui::Render();
+        glClearColor(0.45f, 0.55f, 0.60f, 1.00f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        // Swap buffers
+        glfwSwapBuffers(window);
+    }
+
+    // Cleanup
+    cleanupImGui();
+    glfwDestroyWindow(window);
+    glfwTerminate();
     return 0;
 }
+
+//int main()
+//{
+//    std::cout << "Hello, World!" << std::endl;
+//
+//    return 0;
+//}
